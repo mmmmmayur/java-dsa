637701812a1ae7d29cfb078e771e6d339f04a3fd
Salunkhe Salunkhe <salunkhemayur1823@gmail.com>
2023-10-07 20:34:34 +0530

    enumeration

--- src/prayag.java
+++ src/prayag.java
@@ -1,7 +1,17 @@
-public enum {
+public class prayag {
     //enum is the class where we can make constant which are static .enum ia used for the  fixed variable likw month days ,days,etc
+
+    enum level {
+
     LOW,
     MEDIUM,
-    HIGH
-
+    HIGH;
 }
+
+
+        public static void main(String[] args) {
+            prayag level = level.MEDIUM;
+            System.out.println(myVar);
+        }
+    }
+
